ea8f864c68a799467e61c51077aba68e2eb8b7e0
Samakshya Nanda <nanda.s@ycs.in>
2025-08-24 19:18:46 +0530

    bubble sort optimized with count when count=0 it returns

--- bubble sort/Main.java
+++ bubble sort/Main.java
@@ -12,13 +12,18 @@ class Main {
     
     static void bubbleSort(int[] arr,int length){
             for(int i=0;i<length-1;i++){
+                int count=0;
                 for(int j=0;j<length-i-1;j++){
-                    if(arr[j]>arr[j+1]){
+                      if(arr[j]>arr[j+1]){
+                        count++;
                         int temp=arr[j];
                         arr[j]=arr[j+1];
                         arr[j+1]=temp;
                     }
-                }
+                 }
+                 if (count<1){
+                       return; 
+                    }
             }
     }
 }
