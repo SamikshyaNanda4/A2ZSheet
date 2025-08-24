c845146cdfd4844c877c17744eb3e01991a6746a
Samakshya Nanda <nanda.s@ycs.in>
2025-08-24 19:12:59 +0530

    bubble sorting

--- bubble sort/Main.java
+++ bubble sort/Main.java
@@ -11,8 +11,8 @@ class Main {
     }
     
     static void bubbleSort(int[] arr,int length){
-            for(int i=length-1;i>=1;i--){
-                for(int j=0;j<i;j++){
+            for(int i=0;i<length-1;i++){
+                for(int j=0;j<length-i-1;j++){
                     if(arr[j]>arr[j+1]){
                         int temp=arr[j];
                         arr[j]=arr[j+1];
