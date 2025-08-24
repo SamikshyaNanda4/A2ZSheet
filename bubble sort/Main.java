c8c6a2ae2d56e09571e4dd78165eef7f83fab3a9
Samakshya Nanda <nanda.s@ycs.in>
2025-08-24 19:10:41 +0530

    bubble sort algorithm

---
+++ bubble sort/Main.java
@@ -0,0 +1,24 @@
+//13 46 24 52 20 9
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        int[] arr={13,46,24,52,20,9};
+        System.out.println(Arrays.toString(arr));
+        
+        bubbleSort(arr,arr.length);
+         System.out.println(Arrays.toString(arr));
+    }
+    
+    static void bubbleSort(int[] arr,int length){
+            for(int i=length-1;i>=1;i--){
+                for(int j=0;j<i;j++){
+                    if(arr[j]>arr[j+1]){
+                        int temp=arr[j];
+                        arr[j]=arr[j+1];
+                        arr[j+1]=temp;
+                    }
+                }
+            }
+    }
+}
